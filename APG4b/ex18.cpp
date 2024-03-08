b10f6019fcf9e1b279bf011c3de4a3d2b8b939e7
tegusuke <schunji9@gmail.com>
2024-03-08 22:12:01 +0900

    Create ex18.cpp

---
+++ APG4b/ex18.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<int> w(m), l(m);
+  for (int i = 0; i < m; i++) {
+    cin >> w.at(i) >> l.at(i);
+  }
+  vector<vector<int>> ans(n, vector<int>(n));
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      
+    }
+  }
+}
